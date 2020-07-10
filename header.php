466eeca5b501b3ecbe7b01ac9a874fe42bcd3a03
NGABIRANO Brice <ngablanco234@gmail.com>
2020-07-10 22:58:24 +0200

    Modification design

--- header.php
+++ header.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
     <title>Document</title>
+    
 </head>
 <body>
-    <div class="">
+    <div class="container">
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="a_propos.php">A propos</a></li>
-            <li><a href="quisommes.php">Qui sommes-nous</a></li>
+            <li><a href="apropos.php">A propos</a></li>
+            <li><a href="quissommesnous.php">Qui sommes-nous</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </div>
-    <div class="">
     <div class="">
         <ul>
             <li><a href="politique.php">Politique</a></li>

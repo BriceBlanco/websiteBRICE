b1c9bb814df281864696db5529e88ce08c8234bd
NGABIRANO Brice <ngablanco234@gmail.com>
2020-07-10 06:11:49 +0200

    Ajout de la page header

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hor-menu">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="a_propos.php">A propos</a></li>
+            <li><a href="quisommes.php">Qui sommes-nous</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+    <div class="sections">
+    <div class="vert-menu">
+        <ul>
+            <li><a href="politique.php">Politique</a></li>
+            <li><a href="economie.php">Economie</a></li>
+            <li><a href="sport.php">Sport</a></li>
+            <li><a href="musique.php">Musique</a></li>
+        </ul>
+    </div>
+</body>
+</html>

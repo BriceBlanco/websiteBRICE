35a15ea9c1c6b8e8834830cfaef1a521f5bcccdf
NGABIRANO Brice <ngablanco234@gmail.com>
2020-07-12 20:00:11 +0200

    Integration des class de bootstrap

--- header.php
+++ header.php
@@ -4,23 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
-    <title>Document</title>
+    <title>Accueil</title>
     
 </head>
 <body>
-    <div class="container">
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="apropos.php">A propos</a></li>
-            <li><a href="quisommesnous.php">Qui sommes-nous</a></li>
-            <li><a href="contact.php">Contact</a></li>
-        </ul>
-    </div>
-    <div class="">
-        <ul>
-            <li><a href="politique.php">Politique</a></li>
-            <li><a href="economie.php">Economie</a></li>
-            <li><a href="sport.php">Sport</a></li>
-            <li><a href="musique.php">Musique</a></li>
-        </ul>
-    </div>
+<nav>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Acceuil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="apropos.php">A propos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="actualites.php">Actualites</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="quisommesnous.php">Qui sommes-nous</a>
+        </li>
+    </ul>
+    
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="politique.php">Politique</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="economie.php">Economie</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="sport.php">Sport</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="musique.php">Musique</a>
+        </li>
+    </ul>
+</nav>    
